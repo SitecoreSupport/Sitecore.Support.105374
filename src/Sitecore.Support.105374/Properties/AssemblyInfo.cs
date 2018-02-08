ba7c3792596809cf04b6d4c23f507cade1791e3a
DK\ach <ach@sitecore.net>
2018-02-08 16:10:01 +0200

    Revert "Commit the fix"
    This reverts commit a7d58e47757ad86f1b4a3c0e0f1d8a70f2e8ced3.

--- src/Sitecore.Support.105374/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Sitecore.Support.105374")]
-[assembly: AssemblyProduct("Sitecore.Support.105374")]
-[assembly: ComVisible(false)]
